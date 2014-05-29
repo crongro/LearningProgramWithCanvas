57a051187935e25fcee5d1b2c4654b1538b431a3
root <root@jisunode.(none)>
2014-05-29 19:21:35 +0900

    MOD. refactoring. color 기능 통합

--- canvas.js
+++ canvas.js
@@ -1,8 +1,4 @@
 
-var _d = {
-    color : "magenta",
-    ERR_MSG_FILLCOLOR : "\nfillcolor() 안에 원하시는 색깔을 넣어보세요~ \n pink,red,orange,magenta,green,gray...\n\n기본색으로 magenta로 색칠할게요"
-};
 
 /* DRAW CLASS */
 function DRAW() {
@@ -11,36 +7,45 @@ function DRAW() {
     this._setDefaultStyle();
 }
 
-DRAW.prototype.drawRect = function(x,y,width,height) {
-    //this._ctx.fillRect (x,y,width,height);
-    //this._ctx.strokeRect(x,y,width,height);
-    this._ctx.rect(x,y,width,height);
+DRAW.prototype.strokeRect = function(x,y,width,height, sColor) {
+    this._ctx.strokeStyle = sColor;
+    this._ctx.strokeRect(x,y,width,height);
+}
+
+DRAW.prototype.fillRect = function(x,y,width,height,sColor) {
+    this._ctx.fillStyle = sColor;
+    this._ctx.fillRect(x,y,width,height);
+}
+
+DRAW.prototype.strokeCircle = function(x,y,r,sColor) {
+    //var _nSize = !!r ? (parseInt(r)/180) : 2;
+    //var _arc = Math.PI *_nSize ;
+
+    this._ctx.strokeStyle = sColor;
+    this._ctx.beginPath();
+    this._ctx.arc(x,y,r,0,Math.PI*2);
     this._ctx.stroke();
 }
 
-DRAW.prototype.drawCircle = function(x,y,r,s) {
-    var _nSize = !!s ? (parseInt(s)/180) : 2;
-    var _arc = Math.PI *_nSize ;
-
-    this._ctx.beginPath();
-    this._ctx.arc(x,y,r,0,_arc);
-    this._ctx.stroke();
+DRAW.prototype.fillCircle = function(x,y,r,sColor) {
+    this.strokeCircle(x,y,r,'white');
+    this._ctx.fillStyle = sColor;
+    this._ctx.fill();
 }
 
 DRAW.prototype._setDefaultStyle = function() {
-    this._ctx.fillStyle = "magenta";
+    this._ctx.fillStyle = _d.color; 
+    this._ctx.strokeStyle = _d.blackColor;
 }
 
 DRAW.prototype._getContext = function() {
     return this._ctx;
 }
 
-DRAW.prototype.setColor = function(sColor) {
-    this._ctx.strokeStyle = sColor;
-}
-
-DRAW.prototype.drawTriangle = function(x1,y1,x2,y2,x3,y3) {
+DRAW.prototype.drawTriangle = function(x1,y1,x2,y2,x3,y3,sColor) {
     var ctx = this._ctx;
+
+    ctx.strokeStyle = sColor;
     ctx.beginPath();
     ctx.moveTo(x1,y1);
     ctx.lineTo(x2,y2);
@@ -49,8 +54,8 @@ DRAW.prototype.drawTriangle = function(x1,y1,x2,y2,x3,y3) {
     ctx.stroke();
 }
 
-DRAW.prototype.setFillColor = function(sColor) {
-    sColor = sColor || this._ctx.fillStyle || 'magenta';
+DRAW.prototype.fillTriangle = function(x1,y1,x2,y2,x3,y3,sColor) {
+    this.drawTriangle(x1, y1, x2, y2, x3, y3, sColor);
     this._ctx.fillStyle = sColor;
     this._ctx.fill();
 }
@@ -63,6 +68,22 @@ DRAW.prototype.clearCanvas = function(sColor) {
 }
 
 
+var _d = {
+    color : "white",
+    blackColor : "black",
+    ERR_MSG_FILLCOLOR : "\nfillcolor() 안에 원하시는 색깔을 넣어보세요~ \n pink,red,orange,magenta,green,gray...\n\n기본색으로 magenta로 색칠할게요"
+};
+
+var _util = {
+
+    _getStrokeColor : function (sColor) {
+        return sColor === undefined ? _d.blackColor : sColor;
+    },
+
+    _getFillColor : function (sColor) {
+        return sColor === undefined ? _d.color : sColor;
+    }
+}
 
 /**
  * Public function 
@@ -70,32 +91,41 @@ DRAW.prototype.clearCanvas = function(sColor) {
  */
 
 /* basic Draw */
-function rect(x,y,width,height) {
-    _oPen.drawRect(x,y,width,height);
+function rect(x,y,width,height,sColor) {
+    var sColor = _util._getStrokeColor(sColor); 
+    _oPen.strokeRect(x,y,width,height,sColor);
 }
 
-function color(sColor) {
-    var sColor = sColor === undefined ? _d.color : sColor;
-    _oPen.setColor(sColor);
+function fillrect(x,y,width,height,sColor) {
+    var sColor = _util._getFillColor(sColor); 
+    _oPen.fillRect(x,y,width,height,sColor);
 }
 
-function circle(x,y,r,s) {
-    _oPen.drawCircle(x,y,r,s);
+function circle(x,y,r,sColor) {
+    var sColor = _util._getStrokeColor(sColor); 
+    _oPen.strokeCircle(x,y,r,sColor);
 }
 
-function fillcolor(sColor) {
-    if(!sColor || typeof sColor !== 'string') {
-        alert(_d.ERR_MSG_FILLCOLOR);
-    }
-    _oPen.setFillColor(sColor);
+
+function fillcircle(x,y,r,sColor) {
+    var sColor = _util._getFillColor(sColor); 
+    _oPen.fillCircle(x,y,r,sColor);
 }
 
-function triangle(x1,y1,x2,y2,x3,y3) {
-    _oPen.drawTriangle(x1,y1,x2,y2,x3,y3);
+function triangle(x1,y1,x2,y2,x3,y3,sColor) {
+    var sColor = _util._getStrokeColor(sColor); 
+    _oPen.drawTriangle(x1,y1,x2,y2,x3,y3,sColor);
 }
+
+function filltriangle(x1,y1,x2,y2,x3,y3,sColor) {
+    var sColor = _util._getFillColor(sColor); 
+    _oPen.fillTriangle(x1,y1,x2,y2,x3,y3,sColor);
+}
+
 
 function clear(){
     _oPen.clearCanvas();
 }
 
 var _oPen = new DRAW();
+
